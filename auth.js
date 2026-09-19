9b9b1f70d0ef999624789840b5ecb1cfa8e23853
maihuong040 <daomaihuong040@gmail.com>
2026-09-19 10:46:12 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -15,7 +15,7 @@ function login(username, password) {
     }
 
     // Kiểm tra tài khoản và mật khẩu
-    return username === "admin" && password === "123";
+    return username === "admin" && password === "123567";
 }
 
 module.exports = { login };
