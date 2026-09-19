9e8f2de982982251d476f70083ea974fb8c8c4f9
maihuong040 <daomaihuong040@gmail.com>
2026-09-19 09:59:44 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,21 @@
+function login(username, password) {
+    // Tài khoản bị khóa
+    if (username === "locked") {
+        return false;
+    }
+
+    // Username hoặc password rỗng
+    if (!username || !password) {
+        return false;
+    }
+
+    // Password chứa ký tự đặc biệt
+    if (!/^[a-zA-Z0-9]+$/.test(password)) {
+        return false;
+    }
+
+    // Kiểm tra tài khoản và mật khẩu
+    return username === "admin" && password === "123";
+}
+
+module.exports = { login };
